cb25047bf45aa2ffc9f1068d989fb1d765b8fa83
cpgeier <cpg3rb@virginia.edu>
2017-09-17 03:01:35 -0400

    Fixed l

--- handler.js
+++ handler.js
@@ -2,15 +2,17 @@
 
 exports.handler = (event, context, callback) => {
     // when intent get fulfilled, inform lex to complete the state
+    var purchase = event.currentIntent.name;
     let response = {sessionAttributes: event.sessionAttributes,
       dialogAction: {
         type: "Close",
         fulfillmentState: "Fulfilled",
         message: {
           contentType: "PlainText",
-          content: "Thanks for purchasing book."
+          content: purchase
         }
       }
     }
     callback(null, response);
 };
+
