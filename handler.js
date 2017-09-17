9837b7c7ac3e958b69f92f5026e1e15b52a14a46
elewis9989 <elewis9989@gmail.com>
2017-09-17 01:56:39 -0400

    fixed lambda

--- handler.js
+++ handler.js
@@ -1,40 +1,16 @@
 'use strict';
-var AWS = require('aws-sdk');
-AWS.config.update({region: 'us-east-1'});
 
-module.exports.hello = (event, context, callback) => {
-  //AWS.config.region = 'us-east-1';
-
-  try{
-      const response = {
-        statusCode: 200,
-        body: JSON.stringify({message: "1.20"}),
+exports.handler = (event, context, callback) => {
+    // when intent get fulfilled, inform lex to complete the state
+    let response = {sessionAttributes: event.sessionAttributes,
+      dialogAction: {
+        type: "Close",
+        fulfillmentState: "Fulfilled",
+        message: {
+          contentType: "PlainText",
+          content: "Thanks for purchasing book."
+        }
       }
-    var params = {
-    botAlias: 'dev', /* required */
-    botName: 'TestBot', /* required */
-    inputText: 'Admissions what do i need', /* required */
-    userId: 'USER', /* required */
-    sessionAttributes: {
-      }
-    };
-    returned = lexruntime.postText(params, function(err, data) {
-      if (err) {
-        //console.log(err, err.stack);
-      }  // an error occurred
-      else{
-
-      }                // successful response
-    });
-    response[body] = JSON.stringify(returned);
-    callback(null,response);
-  }
-  catch (err) {
-    callback(null, err);
-  }  
-}
-  //callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-
-
+    }
+    callback(null, response);
+};
